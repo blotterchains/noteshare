49ccc3d831596e3e7d624bac1b346c67ca9dfdf8
wizif <rezagina68@gmail.com>
2020-12-29 20:34:13 +0330

    condition to secure permium books in find function

--- api/src/Core/general/general.py
+++ api/src/Core/general/general.py
@@ -1,20 +1,41 @@
 
+from src.Core.utils import  isIntType
 def index(data,col):
     x=col.insert_one(data)
     return x.inserted_id
 def update(data,col):
+
     myquery = {"_id":data['_id']}
+    # myquery = {"username":data['username']}
     newvalues = { "$set": data }
     col.update_one(myquery,newvalues)
     _ret=[]
-    for x in col.find():
+    for x in col.find(data):
         _ret.append(x)
     return _ret
 def find(data,col):
-    myquery=data
-    limit=data.pop('limit')
+    if('limit' and 'page' in data):
+
+        limit=int(data.pop('limit'))
+        page=int(data.pop('page'))
+    else:
+        return 'plz enter limit and page'
+    dd=data
+    for i in data:
+        if(isIntType(i)):
+            data[i]=int(data[i])
+        else:
+            data[i]={'$regex': '.*%s.*'%data[i]}
+    myquery={
+            "$or":[dd]
+    }
     _ret=[]
-    for x in col.find(data).limit(limit):
+    for x in col.find(myquery).skip(page*limit).limit(limit):
+        x['_id']=str(x['_id'])
+        try:
+            if(x['price']!='free'):
+                x['url']=''
+        except:pass
         _ret.append(x)
     return _ret
 def clear(data,col):
