7ab50ca50d6f57a567cecfbfd6e8e568a4ab3334
wizif <rezagina68@gmail.com>
2020-12-29 20:35:41 +0330

    a change in perviouse routes and we have new pages
    for signup and active after registering and profile api to users can update books or add new books
    and a api to show info about one book with full details

--- api/src/Core/routes.py
+++ api/src/Core/routes.py
@@ -2,12 +2,27 @@ from src.Core.general.general import index
 from src.Core.general.general import clear
 from src.Core.general.general import update
 from src.Core.general.general import find
+from src.Core.user.accounts import login
+from src.Core.user.accounts import signup
+from src.Core.user.accounts import compeleteSignUp
+from src.Core.user.accounts import getUserInfo
+from src.Core.user.books import newBook
+from src.Core.user.books import updateBook
+from src.Core.user.books import getBookInfo
 def dispacther():
 
     dispatch={
         "index":index,
         "clear":clear,
         "update":update,
-        "find":find
+        "find":find,
+        "login":login,
+        "login-userinfo":getUserInfo,
+        
+        "signup":signup,
+        "signup-compelete":compeleteSignUp,
+        "books-newbook":newBook,
+        "books-updatebook":updateBook,
+        "books-info":getBookInfo
     }
     return dispatch
