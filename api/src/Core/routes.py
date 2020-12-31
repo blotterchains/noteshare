bc957a2a3014d01dc79da301723a1306a59daedf
wizif <rezagina68@gmail.com>
2020-12-31 19:50:53 +0330

    2 new routes added
    register orders and comfirm payment of orders added

--- api/src/Core/routes.py
+++ api/src/Core/routes.py
@@ -6,6 +6,8 @@ from src.Core.user.accounts import login
 from src.Core.user.accounts import signup
 from src.Core.user.accounts import compeleteSignUp
 from src.Core.user.accounts import getUserInfo
+from src.Core.user.accounts import buyNewBook
+from src.Core.user.accounts import updateOrdersBook
 from src.Core.user.books import newBook
 from src.Core.user.books import updateBook
 from src.Core.user.books import getBookInfo
@@ -13,15 +15,20 @@ from src.Core.user.books import insertComment
 def dispacther():
 
     dispatch={
+        ###############################general routes
         "index":index,
         "clear":clear,
         "update":update,
         "find":find,
+        ###############################accounts routes
         "login":login,
         "login-userinfo":getUserInfo,
-        
+        "login-order":buyNewBook,
+        "login-updateorder":updateOrdersBook,
+        ###############################signup routes
         "signup":signup,
         "signup-compelete":compeleteSignUp,
+        ###############################books routes
         "books-newbook":newBook,
         "books-updatebook":updateBook,
         "books-info":getBookInfo,
